f3e6b886bc0833060c8f9096de2da37d67ec197a
Your Name <your@email.com>
2025-07-26 10:33:25 +0000

    fix: meeting header

--- components/ui/CallHeader.tsx
+++ components/ui/CallHeader.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 import React, { PropsWithChildren } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useThemeColors } from '@/hooks/useThemeColor'
@@ -6,12 +6,13 @@ import { LinearGradient } from 'expo-linear-gradient'
 
 const CallHeader = ({children}:PropsWithChildren) => {
   const colors = useThemeColors()
+  const {width}=Dimensions.get("window")
   return (
-    <SafeAreaView>   
-   <LinearGradient start={{x:0,y:0}} end={{x:1,y:1}} colors={[colors.gradientStart,colors.gradientMiddle,colors.gradientEnd]} style={{alignItems:'center',flexDirection:"row",borderBottomWidth:StyleSheet.hairlineWidth,borderBottomColor:colors.border,height:50,padding:10,justifyContent:"center"}}>
+    
+   <LinearGradient start={{x:0,y:0}} end={{x:1,y:1}} colors={["#1E2A33","#3C2A4D", "#4A2F36"]} style={{alignItems:'center',flexDirection:"row",borderBottomWidth:StyleSheet.hairlineWidth,borderBottomColor:"#2E2E2E",height:width*0.2,justifyContent:"center"}}>
           {children}
     </LinearGradient>
-    </SafeAreaView>
+    
     
   )
 }
